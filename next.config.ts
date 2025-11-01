e73250744700860726f738093359d2986685f02d
youssef dardeer <youssef.dardeer.mousa@gmail.com>
2025-11-01 14:14:45 +0200

    added vercel.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  swcMinify: true,
+};
 
 const withNextIntl = createNextIntlPlugin('./app/i18n/request.ts');
 export default withNextIntl(nextConfig);
